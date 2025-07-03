f27962db3eb07be188b09e60c0641702592d8f47
amoebajelek <amoebajelek@gmail.com>
2025-07-03 19:26:29 +0700

    Centering content

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-      <div className="">
-        <ModeToggle />
+      <div className="flex">
+        <div className="flex justify-center items-center w-full min-h-screen">
+          <ModeToggle />
+        </div>
       </div>
   );
 }
