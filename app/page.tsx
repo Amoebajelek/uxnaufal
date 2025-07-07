0d2865a6a0a830f292a3c9a69ce4d928d06e6b3f
amoebajelek <amoebajelek@gmail.com>
2025-07-07 21:10:22 +0700

    Fix navbar position

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import { MdOutlineArrowDownward } from "react-icons/md";
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div className="flex px-16 py-8 min-w-full bg-white shadow absolute">
-        <h1 className="text-xl font-bold text-black">uxnaufal</h1>
+      <div className="flex px-16 py-6 min-w-full justify-between bg-white shadow fixed top-0 z-10">
+        <h1 className="text-2xl font-semibold text-black">uxnaufal</h1>
+        <ModeToggle />
       </div>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
         <div className="flex gap-8">
@@ -23,13 +24,13 @@ export default function Home() {
           <MdOutlineArrowDownward size={24} className="text-gray-500" />
         </div>
       </div>
-      <div className="flex gap-4 justify-center items-center">
+      {/* <div className="flex gap-4 justify-center items-center">
         <div className="w-32 h-32"></div>
         <p className="text-xl text-gray-500 w-96 mt-6">
           I’ve been in the design industy since +3 years. I craft digital
           product that are useful & enjoyable for the final users.
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
