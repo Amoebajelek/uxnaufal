8d3ce299d6842a6ed7a8101a8348b7b8d2ad0c40
amoebajelek <amoebajelek@gmail.com>
2025-07-14 00:02:48 +0700

    Change a to Link

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Link from "next/link";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Link2Icon } from "lucide-react";
 
 export default function Navbar() {
   return (
     <div className="flex px-6 lg:px-16 py-6 min-w-full justify-between items-center bg-gray-100 dark:bg-black shadow fixed top-0 z-10">
-      <a href="/" className="cursor-pointer">
+      <Link href="/" className="cursor-pointer">
         <h1 className="text-2xl font-semibold text-black dark:text-white">
           uxnaufal
         </h1>
-      </a>
+      </Link>
       <ModeToggle />
     </div>
   );
