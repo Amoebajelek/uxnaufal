1a065e6192ae789864ae22889586041fc1c27a5d
amoebajelek <amoebajelek@gmail.com>
2025-07-13 23:30:54 +0700

    Add contents

--- app/portofolios/redesign-aitiserve-official-website/page.tsx
+++ app/portofolios/redesign-aitiserve-official-website/page.tsx
@@ -4,13 +4,19 @@ import Image from "next/image";
 import Navbar from "@/app/_components/Navbar";
 import { MdOutlineArrowRightAlt } from "react-icons/md";
 import Papyrusphoto from "@/public/Thumbnail_Papyrusphoto.jpg";
+import PreviewProblemDiscovery from "@/public/Preview-Problem-Discovery.jpg";
+import DesignThinkingAitiserve from "@/public/Design-Thinking-Aitiserve.jpg";
+import WireframeAitiserve from "@/public/Wireframe-Aitiserve.jpg";
+import VisualDesignAitiserve from "@/public/Visual-Design-Aitiserve.jpg";
+import Footer from "@/app/_components/Footer";
 
 export default function page() {
   return (
     <div className="flex flex-col">
       <Navbar />
+      {/* Start Header */}
       <div className="flex flex-col items-center min-h-screen p-6 bg-gray-100 dark:bg-black mt-24">
-        <div className="flex flex-col w-full lg:w-1/2">
+        <div className="flex flex-col w-full lg:w-8/12">
           <div className="flex gap-2 w-full mb-12 items-center cursor-pointer text-gray-500 hover:text-gray-800">
             <MdOutlineArrowRightAlt
               size={24}
@@ -63,7 +69,6 @@ export default function page() {
               </p>
             </div>
           </div>
-
           <div className="flex gap-16 mt-8">
             <div className="flex flex-col">
               <p className="text-lg text-gray-500 dark:text-gray-500">
@@ -94,23 +99,26 @@ export default function page() {
               </a>
             </div>
           </div>
-          <div className="flex mt-8 gap-4 w-full">
-            {/* Left Side */}
-            <div className="flex flex-col gap-1 text-lg text-gray-500 dark:text-gray-500 w-1/5">
-              <p>In a Nutshell</p>
-              <p>Context</p>
-              <p>Problem Discovery</p>
-              <p>Problem #1</p>
-              <p>Solution #1</p>
-              <p>Problem #2</p>
-              <p>Solution #2 #2</p>
-              <p>Full Design</p>
-              <p>Reflection</p>
+          {/* End Header */}
+          <div className="flex gap-4 w-full h-full mt-8">
+            {/* Start Left-Side */}
+            <div className="hidden md:flex flex-col w-2/8 min-h-full relative bg-gray-100">
+              <div className="flex flex-col gap-1.5 text-lg text-gray-500 dark:text-gray-500 top-32 sticky">
+                <p>In a Nutshell</p>
+                <p>Context</p>
+                <p>Problem Discovery</p>
+                <p>Problem #1</p>
+                <p>Solution #1</p>
+                <p>Problem #2</p>
+                <p>Solution #2</p>
+                <p>Full Design</p>
+                <p>Reflection</p>
+              </div>
             </div>
-
-            {/* Right Side */}
-            <div className="flex flex-col gap-4 w-4/5">
-              {/* Content 1 */}
+            {/* End Left-Side */}
+            {/* Right-Content 1 */}
+            <div className="flex flex-col gap-4 w-6/8">
+              {/* Start Right-Content : In a Nutshell */}
               <div className="flex flex-col">
                 <p className="text-lg text-gray-500 dark:text-gray-500">
                   In a Nutshell
@@ -123,6 +131,21 @@ export default function page() {
               <p className="text-lg text-black dark:text-white">
                 Enhancing the AITISERVE website to meet stakeholder requirements
                 and support the company&apos;s long-term objectives.
+              </p>
+              {/* End Right-Content : In a Nutshell */}
+              {/* Start Right-Content : Context */}
+              <div className="flex flex-col">
+                <p className="text-lg text-gray-500 dark:text-gray-500">
+                  Context
+                </p>
+                <h1 className="text-2xl font-bold text-black dark:text-white uppercase">
+                  Understanding AITISERVE
+                </h1>
+              </div>
+              <p className="text-lg text-black dark:text-white">
+                AITISERVE specializes in software development and digital
+                platform services, including programming, web portal management,
+                as well as IT consulting and technical support.
               </p>
               <div className="w-full max-h-[600px] bg-black dark:bg-gray-100 rounded-3xl lg:rounded-4xl overflow-hidden my-4">
                 <Image
@@ -171,10 +194,195 @@ export default function page() {
                   </li>
                 </ul>
               </div>
+              {/* End Right-Content : Context */}
+              {/* Start Right-Content : Problem Discovery */}
+              <div className="flex flex-col">
+                <p className="text-lg text-gray-500 dark:text-gray-500">
+                  Problem Discovery
+                </p>
+                <h1 className="text-2xl font-bold text-black dark:text-white uppercase">
+                  The current design feels visually underwhelming and could be
+                  improved for better engagement.
+                </h1>
+              </div>
+              <p className="text-lg text-black dark:text-white">
+                The design feels unpolished, with cluttered content,
+                inconsistent layout, and missing documentation. Despite being
+                new, establishing professionalism early on is crucial.
+              </p>
+              {/* End Right-Content : Context */}
+              {/* Start Right-Content : Problem #1 */}
+              <div className="flex flex-col">
+                <p className="text-lg text-gray-500 dark:text-gray-500">
+                  Problem #1
+                </p>
+                <h1 className="text-2xl font-bold text-black dark:text-white uppercase">
+                  Weak visual presentation
+                </h1>
+              </div>
+              <p className="text-lg text-black dark:text-white">
+                Inconsistent alignment across components, content, typography,
+                and layout affects overall visual harmony.
+              </p>
+              <div className="flex flex-col gap-2">
+                <p className="text-lg font-semibold text-black dark:text-white">
+                  Preview
+                </p>
+                <div className="w-full h-fit bg-black dark:bg-gray-100 rounded-lg overflow-hidden my-4">
+                  <Image
+                    src={PreviewProblemDiscovery}
+                    alt="Preview Problem Discovery"
+                    width={1600}
+                  />
+                </div>
+              </div>
+              {/* End Right-Content : Problem #1 */}
+              {/* Start Right-Content : Solution #1 */}
+              <div className="flex flex-col">
+                <p className="text-lg text-gray-500 dark:text-gray-500">
+                  Solution #1
+                </p>
+                <h1 className="text-2xl font-bold text-black dark:text-white uppercase">
+                  Using UX laws to guide decision-making in new design
+                  development.
+                </h1>
+              </div>
+              <p className="text-lg text-black dark:text-white">
+                In the redesign process, we applied foundational UX laws—such as
+                Hick’s Law, Fitts’s Law, and the Law of Proximity—to ensure
+                every design decision was backed by human-centered principles.
+                These laws helped streamline user interactions, improve visual
+                hierarchy, and create more intuitive experiences. By grounding
+                our design choices in proven UX principles, we enhanced
+                usability, clarity, and overall user satisfaction.
+              </p>
+              {/* End Right-Content : Solution #1 */}
+              {/* Start Right-Content : Problem #2 */}
+              <div className="flex flex-col">
+                <p className="text-lg text-gray-500 dark:text-gray-500">
+                  Problem #2
+                </p>
+                <h1 className="text-2xl font-bold text-black dark:text-white uppercase">
+                  Design documentation is missing or incomplete, making
+                  implementation harder.
+                </h1>
+              </div>
+              <p className="text-lg text-black dark:text-white">
+                The absence or incompleteness of design documentation creates
+                significant challenges during the development phase. Without
+                clear references for components, layouts, and interactions,
+                developers may misinterpret the intended design, leading to
+                inconsistencies, delays, and increased rework. Comprehensive
+                documentation is essential to ensure smooth collaboration,
+                accurate implementation, and long-term maintainability.
+              </p>
+              {/* End Right-Content : Problem #2 */}
+              {/* Start Right-Content : Solution #2 */}
+              <div className="flex flex-col">
+                <p className="text-lg text-gray-500 dark:text-gray-500">
+                  Solution #2
+                </p>
+                <h1 className="text-2xl font-bold text-black dark:text-white uppercase">
+                  Applying the Design Thinking framework through its five key
+                  phases.
+                </h1>
+              </div>
+              <p className="text-lg text-black dark:text-white">
+                The Design Thinking framework was applied systematically through
+                its five key phases: Empathize, Define, Ideate, Prototype, and
+                Test. This approach ensured a deep understanding of user needs,
+                clear problem definition, creative solution generation,
+                iterative prototyping, and continuous validation. By following
+                this user-centered process, we were able to design solutions
+                that are not only functional but also meaningful and relevant to
+                real user challenges.
+              </p>
+              {/* End Right-Content : Solution #2 */}
+              {/* Start Right-Content : Final Design */}
+              <div className="flex flex-col">
+                <p className="text-lg text-gray-500 dark:text-gray-500">
+                  Final Design
+                </p>
+                <h1 className="text-2xl font-bold text-black dark:text-white uppercase">
+                  The new look of AITISERVE
+                </h1>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-lg font-semibold text-black dark:text-white">
+                  Design Thinking
+                </p>
+                <div className="w-full h-fit bg-black dark:bg-gray-100 rounded-lg overflow-hidden my-4">
+                  <Image
+                    src={DesignThinkingAitiserve}
+                    alt="Design Thinking Aitiserve"
+                    width={1600}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-lg font-semibold text-black dark:text-white">
+                  Low-Fidelity / Wireframe
+                </p>
+                <div className="w-full h-fit bg-black dark:bg-gray-100 rounded-lg overflow-hidden my-4">
+                  <Image
+                    src={WireframeAitiserve}
+                    alt="Wireframe Aitiserve"
+                    width={1600}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-lg font-semibold text-black dark:text-white">
+                  high-Fidelity / Visual Design
+                </p>
+                <div className="w-full h-fit bg-black dark:bg-gray-100 rounded-lg overflow-hidden my-4">
+                  <Image
+                    src={VisualDesignAitiserve}
+                    alt="Visual Design Aitiserve"
+                    width={1600}
+                  />
+                </div>
+              </div>
+              {/* End Right-Content : Final Design */}
+              {/* Start Right-Content : Reflection */}
+              <div className="flex flex-col">
+                <p className="text-lg text-gray-500 dark:text-gray-500">
+                  Reflection
+                </p>
+                <h1 className="text-2xl font-bold text-black dark:text-white uppercase">
+                  What I've Learnt & Special Thanks
+                </h1>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-lg text-black dark:text-white">
+                  During this project, I've got the opportunity to:
+                </p>
+                <ul className="list-disc ms-8 text-lg text-black dark:text-white space-y-1">
+                  <li>Learn more about Design Thinking and Product Design.</li>
+                  <li>Conducted Live Usability Tests.</li>
+                  <li>
+                    Conducted A/B Experiment and having the new designs win
+                  </li>
+                </ul>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-lg font-semibold text-black dark:text-white">
+                  Shoutout to all collaborators
+                </p>
+                <p className="text-lg text-black dark:text-white">
+                  Huge shoutout to everyone I collaborated with during this
+                  project
+                </p>
+                <ul className="list-disc ms-8 text-lg text-black dark:text-white space-y-1">
+                  <li>Director of Aitiserve - Achmad Faizal</li>
+                </ul>
+              </div>
+              {/* End Right-Content : Reflection */}
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
