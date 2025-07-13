9ff12344dc9a48bfa45f7b9da23c8780f6f82e8c
amoebajelek <amoebajelek@gmail.com>
2025-07-13 11:05:09 +0700

    Add link redirect to home

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "@/components/mode-toggle";
 export default function Navbar() {
   return (
     <div className="flex px-6 lg:px-16 py-6 min-w-full justify-between items-center bg-gray-100 dark:bg-black shadow fixed top-0 z-10">
-      <a href="/">
+      <a href="/" className="cursor-pointer">
         <h1 className="text-2xl font-semibold text-black dark:text-white">
           uxnaufal
         </h1>
